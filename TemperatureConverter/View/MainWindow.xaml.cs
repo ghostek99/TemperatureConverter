5b3a7f2e89f334af3b80a74331a459d1cd2ebba8
Szymon Nidecki <szymon.nidecki@student.ucll.be>
2021-03-01 10:58:34 +0100

    fahrenheit-binding

--- TemperatureConverter/View/MainWindow.xaml.cs
+++ TemperatureConverter/View/MainWindow.xaml.cs
@@ -25,36 +25,6 @@ namespace View
         {
             InitializeComponent();
         }
-
-        private void ConvertCelsius(object sender, RoutedEventArgs e)
-        {
-            string celsius = celsiusTextBox.Text;
-            var val = double.Parse(celsius);
-            var fahrenheit = (val * 9) / 5 + 32;
-            var kelvin = val + 273.15;
-            fahrenheitTextBox.Text = fahrenheit.ToString();
-            kelvinTextBox.Text = kelvin.ToString();
-        }
-
-        private void ConvertFahrenheit(object sender, RoutedEventArgs e)
-        {
-            string fahrenheit = fahrenheitTextBox.Text;
-            var val = double.Parse(fahrenheit);
-            var celsius = (val - 32) * 5 / 9;
-            var kelvin = celsius + 273.15;
-            celsiusTextBox.Text = celsius.ToString();
-            kelvinTextBox.Text = kelvin.ToString();
-        }
-
-        private void SliderValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
-        {
-            string kelvin = kelvinTextBox.Text;
-            var val = double.Parse(kelvin);
-            var celsius = val - 273.15;
-            var fahrenheit = (celsius * 9) / 5 + 32;
-            celsiusTextBox.Text = celsius.ToString();
-            fahrenheitTextBox.Text = fahrenheit.ToString();
-        }
     }
 
     public class CelsiusConverter : IValueConverter
@@ -71,6 +41,25 @@ namespace View
         {
             var celsius = double.Parse((string)value);
             var kelvin = celsius + 273.15;
+
+            return kelvin;
+        }
+    }
+
+    public class FahrenheitConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var kelvin = (double)value;
+            var fahrenheit = (kelvin - 273.15) * 18 / 10 + 32;
+
+            return fahrenheit.ToString();
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var fahrenheit = double.Parse((string)value);
+            var kelvin = 273.15 + ((fahrenheit - 32.0) * (5.0 / 9.0));
 
             return kelvin;
         }
