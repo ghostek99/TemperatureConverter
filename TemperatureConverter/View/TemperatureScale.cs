4172be8df0467a750d69e25be7dc11b72f7b595a
ghostek99 <szymon.nidecki@student.ucll.be>
2021-03-13 11:14:56 +0100

    kelvin-temperature-scale

--- TemperatureConverter/View/TemperatureScale.cs
+++ TemperatureConverter/View/TemperatureScale.cs
@@ -12,4 +12,24 @@ namespace View
         double ConvertToKelvin(double temperature);
         double ConvertFromKelvin(double temperature);
     }
+
+    class KelvinTemperatureScale : ITemperatureScale
+    {
+        public KelvinTemperatureScale()
+        {
+            this.Name = "KelvinTemperatureScale";
+        }
+
+        public string Name { get; }
+
+        public double ConvertFromKelvin(double temperature)
+        {
+            return temperature;
+        }
+
+        public double ConvertToKelvin(double temperature)
+        {
+            return temperature;
+        }
+    }
 }
